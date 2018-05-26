78cbfdee5e1f3dd159a7f25f933824cd6480a13d
Miguel Pérez Colom <miguelperezcolom@gmail.com>
2018-05-26 13:49:43 +0200

    cambio 3, de momento solo commit

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        System.out.println("Hola 4!!!!");
+        System.out.println(new Helper().test());
 
     }
 }
